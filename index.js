d2666bcbc20b158d692a3e938de65c5d3d3952cd
navjot turna <shamsherturna101@gmail.com>
2021-10-16 23:51:09 -0400

    class data

--- index.js
+++ index.js
@@ -5,3 +5,37 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
 let teamMembers = [];
+
+function runPrompter() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "managerName",
+            message: "what is the team manager's name?"
+        },
+        {
+            type: "input",
+            name: "mangerid",
+            message: "What is the team manager's id?"
+        },
+        {
+            type: "input",
+            name: "managerEmail",
+            message: "What is the team manager's email?"
+        },
+        {
+            type: "input",
+            name: "managerOfficenumber",
+            message: "What is the team manager's office number?"
+        },
+       
+ 
+
+    ]).then((response) => {
+       
+        const manager = new Manager(response.managerName, response.mangerid, response.managerOfficenumber)
+        teamMembers.push(manager);
+    }
+    )
+}
+runPrompter()
