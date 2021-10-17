29335bc83cb3a919c2bbae5e73c33f88aaef38d0
navjot turna <shamsherturna101@gmail.com>
2021-10-17 00:15:38 -0400

    engineer prompts

--- index.js
+++ index.js
@@ -28,14 +28,79 @@ function runPrompter() {
             name: "managerOfficenumber",
             message: "What is the team manager's office number?"
         },
-       
- 
+
+
 
     ]).then((response) => {
-       
+
         const manager = new Manager(response.managerName, response.mangerid, response.managerOfficenumber)
         teamMembers.push(manager);
+        optionprompt();
     }
     )
 }
 runPrompter()
+
+
+// oprtions prompt
+function optionprompt() {
+    inquirer.prompt(
+        [
+            {
+                type: "list",
+                name: "team",
+                message: "WWhich type of team member would you like to add?",
+                choices: ["Enginner", "intern", "i dont want to have team member"],
+            }
+
+        ]
+    ).then((response => {
+        console.log("enter")
+        if (response.team === "Engineer") {
+            console.log("second")
+            enginnerprompt()
+        }
+        else if(response.team === "Intern"){
+            console.log("hello")
+        }
+    }))
+}
+
+// enginner prompt
+function enginnerprompt(){
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "engineer",
+            message: "what is the team Engineer's name?"
+        },
+        {
+            type: "input",
+            name: "eid",
+            message: "What is the team Engineer's id?"
+        },
+        {
+            type: "input",
+            name: "eemail",
+            message: "What is the team Engineer's email?"
+        },
+        {
+            type: "input",
+            name: "egithub",
+            message: "What is the team github's email?"
+
+        },
+
+
+
+    ]
+
+
+
+    ).then((response => {
+        const engineer = new Engineer(response.engineer, response.eid, response.eemail)
+        teamMembers.push(engineer)
+      
+        optionprompt();
+    }))
+}
